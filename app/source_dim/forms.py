88e48d610b67cdbcabf13b803c61aeefcae106ae
Bollinger <PEB0601@daugherty.com>
2019-10-30 14:37:14 -0500

    Added field validation messages and removed the appearance of input boxes in the delete modal.

--- app/source_dim/forms.py
+++ app/source_dim/forms.py
@@ -6,8 +6,8 @@ from wtforms.validators import DataRequired
 class Source_DimForm(FlaskForm):
 
   # Form for admin to add or edit a department
-  name = StringField('Name', validators=[DataRequired()])
-  description = StringField('Description', validators=[DataRequired()])
-  parent_id = SelectField("Parent Id", coerce=int, validators=[DataRequired()])
-  score = SelectField("Score", coerce=int, validators=[DataRequired()])
+  name = StringField('Name', validators=[DataRequired(message="Name is required")])
+  description = StringField('Description', validators=[DataRequired(message="Description is required")])
+  parent_id = SelectField("Parent Id", coerce=int, validators=[DataRequired(message="Id is required")])
+  score = SelectField("Score", coerce=int, validators=[DataRequired(message="Score is required")])
   submit = SubmitField('Submit')
