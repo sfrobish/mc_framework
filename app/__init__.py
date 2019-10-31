baf33523db6c19174214a1dfec080c0df81aa030
sfrobish <s.frobish@gmail.com>
2019-10-31 09:03:47 -0500

    Fix app init config

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ def create_app(config_name):
 
   app = Flask(__name__, instance_relative_config=True)
   app.config.from_object(app_config[config_name])
-  app.config.from_pyfile('config.py')
+  #app.config.from_pyfile('config.py')
   
   Bootstrap(app)
   csrf.init_app(app)
