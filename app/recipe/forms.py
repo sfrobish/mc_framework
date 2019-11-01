c183e5e17d7eac98612a9c00f0a65bf4fdab3ed9
joeklein1204 <joe.klein@daugherty.com>
2019-11-01 07:51:06 -0500

    added recipes

--- app/recipe/forms.py
+++ app/recipe/forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired
 class RecipeForm(FlaskForm):
 
   # Form for admin to add or edit a department
-  name = StringField('Name', validators=[DataRequired()])
-  description = StringField('Description', validators=[DataRequired()])
+  name = StringField('Name', validators=[DataRequired(DataRequired(message="Name is invalid"))])
+  description = StringField('Description', validators=[DataRequired(message="Description is invalid")])
   submit = SubmitField('Submit')
