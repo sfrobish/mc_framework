5a3e7b356ddc71e38a6f356f77b6ed49f41088a7
sfrobish <s.frobish@gmail.com>
2019-12-03 09:14:54 -0600

    make recipe edits update control_recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,20 +18,24 @@ from ..helpers import confirm_user_is_admin
 def list_recipes():
 
   confirm_user_is_admin()
-  
-  # List all recipes
-  recipeslist = recipedbo.query.order_by(asc(recipedbo.recipe_name)).all()
-  controlslist = controldbo.query.order_by(asc(controldbo.control_name)).all()
-  controlrecipeslist = controlrecipedbo.query.all()
 
-  for recipe in recipeslist:
-    matching_controlrecipes = filter(lambda x: x.recipe_id == recipe.recipe_id, controlrecipeslist)
-    selected_control_ids = [x.control_id for x in matching_controlrecipes]
-    setattr(recipe, "selected_control_ids", selected_control_ids)
+  sql = "select array_agg(c.control_name) as control_name_list, array_agg(c.control_id) as control_id_list, \
+                r.recipe_id, r.recipe_name, r.recipe_description \
+         from mc_demo.recipe r \
+         left join mc_demo.control_recipe cr \
+         on r.recipe_id = cr.recipe_id \
+         left join mc_demo.control c \
+         on cr.control_id = c.control_id \
+         group by r.recipe_id"
+  result = db.engine.execute(sql)
+  recipelist = [ row for row in result ]
+  
+  # List all controls for filling modals
+  controlslist = controldbo.query.order_by(asc(controldbo.control_name)).all()
 
   page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-  total = len(recipeslist)
-  pagination_recipes = recipeslist[offset: offset + per_page]
+  total = len(recipelist)
+  pagination_recipes = recipelist[offset: offset + per_page]
   pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
 
   return render_template('recipe/recipe_template.html',
@@ -40,7 +44,7 @@ def list_recipes():
                            pagination=pagination,
                            page=page,
                            per_page=per_page,
-                           title="Controls")
+                           title="Recipes")
 
 
 @recipe.route('/recipes/add', methods=['GET', 'POST'])
@@ -87,9 +91,38 @@ def edit_recipe(id):
   recipedata = recipedbo.query.get_or_404(id)
 
   form = RecipeForm(obj=recipedata)
-  #if form.validate_on_submit():
-  recipedata.recipe_name = form.name.data
-  recipedata.recipe_description = form.description.data
+  recipedict = request.json
+
+  recipedata.recipe_name = recipedict["recipe_name"]
+  recipedata.recipe_description = recipedict["recipe_desc"]
+
+  recipedict["control_list"] = [ int(x) for x in recipedict["control_list"] ]
+  
+  # See if there is a change in controls
+  sql = "select c.recipe_id, array_agg(c.control_id) as control_id_list \
+         from mc_demo.control_recipe c \
+         where c.recipe_id = " + str(id) + " \
+         group by c.recipe_id"
+  print(sql)
+  result = db.engine.execute(sql)    
+  curcontrollist = [ row[1] for row in result ]
+  print(recipedict["control_list"])
+  print(curcontrollist)
+  if len(curcontrollist) == 0:
+    curcontrollist = [ [] ]
+  if curcontrollist[0] == recipedict["control_list"]:
+    print("No change to control list")
+  else:
+    # Delete all of the current controls in recipe
+    delsql = "delete from mc_demo.control_recipe where recipe_id = " + str(id)
+    db.engine.execute(delsql)
+
+    # Insert new controllist
+    for ctl in recipedict["control_list"]:
+      insql = "insert into mc_demo.control_recipe (recipe_id, control_id) values (" + str(id) + "," + str(ctl) + ")"
+      print(insql)
+      db.engine.execute(insql)
+
   db.session.commit()
   flash('You have successfully edited the recipe.')
 
